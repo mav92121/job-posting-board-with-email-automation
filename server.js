d6ed1e44bc57247458420f8240e64484521bee89
devobase-ninja <dev.devobase@gmail.com>
2024-10-18 19:29:33 +0530

    database setup

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import connectDB from "./config/database.js";
 
 const app = express();
+app.use(express.json());
+
+connectDB();
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
